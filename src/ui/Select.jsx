b5b9f617784203ef3c8e3a5ea5be67ca7db861f8
Daniel <daniel@foxm.xyz>
2022-08-11 12:21:50 -0500

    Build the Select item

--- src/ui/Select.jsx
+++ src/ui/Select.jsx
@@ -1,14 +1,35 @@
 import styles from './Select.module.css';
+import ChevronIcon from '../assets/chevron-down.svg'
 import { useCalculation } from "../contexts/Calculation";
-import { createSignal } from 'solid-js';
+import { createRenderEffect, createSignal } from 'solid-js';
 
 function Select({ options }) {
-  const [calculation, { changeCommitment }] = useCalculation();
+  const [calculation, { changeWeeks }] = useCalculation();
+
+  function model(el, value) {
+    const [field, name, setField] = value();
+    console.log({ field, setField, el, value })
+    createRenderEffect(() => {
+      el.value = field[name];
+    });
+    el.addEventListener("input", (e) => setField(e.target.value));
+  }
+
+  console.log(ChevronIcon);
 
   return (
-    <div>
-
-    </div>
+    <select
+      class={styles.Select}
+      disabled={!calculation.commitment}
+      style={{ "background-image": `url("${ChevronIcon}")` }}
+      use:model={[calculation, 'weeks', changeWeeks]}
+    >
+      <option value="0" disabled>Select an option</option>
+      <option value="1">Volvo</option>
+      <option value="2">Saab</option>
+      <option value="3">Mercedes</option>
+      <option value="4">Audi</option>
+    </select>
   );
 }
 
